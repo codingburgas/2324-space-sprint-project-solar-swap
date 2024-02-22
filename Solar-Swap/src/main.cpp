8a1b6fc3810b964fddb131eddc085581c97719ce
GSStoyanov22 <GSStoyanov22@codingburgas.bg>
2024-02-22 21:18:20 +0200

    Add calculate speed class

---
+++ Solar-Swap/src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include <cmath>
+
+#include <cstdlib>
+
+#include <ctime>
+
+
+using namespace std;
+
+
+float calculateSpeed(float force, float mass, float time) {
+
+    // Calculate acceleration using Newton's second law
+
+    float acceleration = force / mass;
+
+    // Calculate displacement assuming starting from rest
+
+    float displacement = 0.5f * acceleration * time * time;
+
+    // Calculate final velocity using the equation of motion
+
+    float velocity = sqrt(2 * acceleration * displacement);
+
+    return velocity;
+
+}
+
+int main() {
+
+    
+}
